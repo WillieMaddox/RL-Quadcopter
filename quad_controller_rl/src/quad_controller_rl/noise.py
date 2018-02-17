d9fe8b17967145a5a0f19d6a271ebff668178873
WillieMaddox <willie.maddox@gmail.com>
2018-02-17 15:24:33 -0600

    minor refactoring

--- quad_controller_rl/src/quad_controller_rl/noise.py
+++ quad_controller_rl/src/quad_controller_rl/noise.py
@@ -30,10 +30,9 @@ class AdaptiveParamNoiseSpec(object):
 
     def __init__(self, initial_stddev=0.1, desired_action_stddev=0.1, adoption_coefficient=1.01):
         self.initial_stddev = initial_stddev
+        self.current_stddev = initial_stddev
         self.desired_action_stddev = desired_action_stddev
         self.adoption_coefficient = adoption_coefficient
-
-        self.current_stddev = initial_stddev
 
     def adapt(self, distance):
         if distance > self.desired_action_stddev:
